0b3681c9c197a9df22daaa9afdfc3d0c18683139
Chad Boyce <chadboyce@gmail.com>
2021-02-21 15:25:17 +0000

    added env vars for gitpod

--- urs/Credentials.py
+++ urs/Credentials.py
@@ -3,9 +3,9 @@
 #===============================================================================
 
 API = {
-    "client_id": "14_CHAR_HERE",        # Personal use script (14 characters).
-    "client_secret": "27_CHAR_HERE",    # Secret key (27 characters).
-    "user_agent": "APP_NAME_HERE",      # App name.
-    "username": "REDDIT_USERNAME_HERE", # Reddit username.
-    "password": "REDDIT_PASSWORD_HERE"  # Reddit password.
+    "client_id": "$CLIENT_ID",        # Personal use script (14 characters).
+    "client_secret": "$CLIENT_SECRET",    # Secret key (27 characters).
+    "user_agent": "$USER_AGENT",      # App name.
+    "username": "$REDDIT_USERNAME", # Reddit username.
+    "password": "$REDDIT_PASSWORD"  # Reddit password.
 }
